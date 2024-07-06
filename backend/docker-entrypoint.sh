beed64407d8cb479ef0c45cd5e3ba00710350d16
Étienne Gauvin <etienne@gauvin.info>
2024-07-06 19:51:31 +0200

    Switch to rsync for node_modules copy

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Copying node_modules..."
-cp -r /packages-cache/node_modules /backend/node_modules
+mkdir -p /backend/node_modules
+rsync --archive --quiet --stats --delete /packages-cache/node_modules/ /backend/node_modules
+# cp -r /packages-cache/node_modules /backend/node_modules
 cd /backend
 npm run start:dev
--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Copying node_modules..."
-cp -r /packages-cache/node_modules /frontend/node_modules
+mkdir -p /frontend/node_modules
+rsync --archive --quiet --stats --delete /packages-cache/node_modules/ /frontend/node_modules
+# cp -r /packages-cache/node_modules /frontend/node_modules
 cd /frontend
 npm run dev
