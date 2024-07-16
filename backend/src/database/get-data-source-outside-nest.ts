2426bc195934e21573201aa0f04cbe571689b551
Étienne Gauvin <etienne@gauvin.info>
2024-07-17 01:57:44 +0200

    File renaming and simplification

--- backend/src/database/get-data-source-outside-nest.ts
+++ backend/src/database/get-data-source-outside-nest.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { DataSource } from "typeorm";
 import { AppModule } from "../app.module.js";
-import { dataSourceOptionsFactory } from "../database/database.providers.js";
 import { TypedConfigService } from "../typed-config/typed-config.service.js";
+import { dataSourceOptionsFactory } from "./data-source-options-factory.js";
 
-export async function getDataSourceOutsideNest(): Promise<DataSource> {
+async function getDataSourceOutsideNest(): Promise<DataSource> {
   const app = await NestFactory.createApplicationContext(AppModule);
   const config = app.get(TypedConfigService);
   const dataSource = new DataSource(dataSourceOptionsFactory(config));
   await app.close();
   return dataSource;
 }
+
+export default await getDataSourceOutsideNest();
