2b89389a4f5c5fc9b2ff38131da7d13a8542307a
Étienne Gauvin <etienne@gauvin.info>
2024-07-17 12:09:13 +0200

    Added routes Playlists

--- backend/src/playlist/playlist.interfaces.ts
+++ backend/src/playlist/playlist.interfaces.ts
@@ -1,5 +1,14 @@
+import Joi from "joi";
 
 export enum PlaylistType {
   Syncthing = 'syncthing',
   Items = 'items',
 }
+
+export interface PutPlaylist {
+  name: string;
+}
+
+export const PutPlaylistSchema = Joi.object<PutPlaylist>({
+  name: Joi.string().max(128),
+})
--- backend/src/playlist/playlist.service.ts
+++ backend/src/playlist/playlist.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Playlist } from './playlist.entity.js';
-import { PlaylistType } from './playlist.interfaces.js';
+import { PlaylistType, PutPlaylist } from './playlist.interfaces.js';
 
 @Injectable()
 export class PlaylistService {
@@ -11,6 +11,34 @@ export class PlaylistService {
   ) { }
 
   async getSyncthingPlaylist(): Promise<Playlist> {
-    return this.playlistRepository.findOneOrFail({ where: { type: PlaylistType.Syncthing } });
+    return this.playlistRepository.findOneOrFail({
+      where: { type: PlaylistType.Syncthing }, select: {
+        id: true,
+        name: true,
+        items: {
+          id: true
+        }
+      }
+    });
+  }
+
+  async getPlaylist(playlist_id: number): Promise<Playlist> {
+    return this.playlistRepository.findOneOrFail({
+      where: { id: playlist_id }, select: {
+        id: true,
+        name: true,
+        items: {
+          id: true
+        }
+      }
+    });
+  }
+
+  async putPlaylist(id: number, data: PutPlaylist): Promise<void> {
+    const result = await this.playlistRepository.update({ id }, data);
+
+    if (!result.affected) {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
   }
 }
