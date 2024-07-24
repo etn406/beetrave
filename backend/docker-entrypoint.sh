65e33b6d7c978deed45fcb6f3ad0fbebc1b815e9
Étienne Gauvin <etienne@gauvin.info>
2024-07-24 16:00:39 +0200

    Removed Nest and Next apps

--- backend/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Copying node_modules..."
-mkdir -p /backend/node_modules
-# The ending slash "/" is *important* on this next line path "/packages-cache/node_modules/":
-rsync --archive --quiet --stats --delete /packages-cache/node_modules/ /backend/node_modules
-# cp -r /packages-cache/node_modules /backend/node_modules
-cd /backend
-npm run start:dev
--- frontend/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Copying node_modules..."
-mkdir -p /frontend/node_modules
-rsync --archive --quiet --stats --delete /packages-cache/node_modules/ /frontend/node_modules
-# cp -r /packages-cache/node_modules /frontend/node_modules
-cd /frontend
-npm run dev
