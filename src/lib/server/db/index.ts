6fa80d338e6b51e303bc2acb547b719535b17131
Étienne Gauvin <etienne@gauvin.info>
2024-07-25 17:10:16 +0200

    Fixed an issue with env variable

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,7 +5,7 @@ import { z } from 'zod';
 
 const client = postgres({
   host: z.string().parse(env.POSTGRES_DATABASE_HOST),
-  port: z.number().parse(Number.parseInt(process.env.POSTGRES_DATABASE_PORT ?? '')),
+  port: z.number().parse(Number.parseInt(env.POSTGRES_DATABASE_PORT ?? '')),
   user: z.string().parse(env.POSTGRES_DATABASE_USER),
   password: z.string().parse(env.POSTGRES_DATABASE_PASSWORD),
   database: z.string().parse(env.POSTGRES_DATABASE_NAME),
