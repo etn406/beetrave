db58281808ed6ebc215c14b2a9f7ab2e441ffe04
Étienne Gauvin <etienne@gauvin.info>
2024-07-25 15:26:20 +0200

    Changes in config

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,34 +1,47 @@
-import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
-import ts from 'typescript-eslint';
+import stylisticTs from '@stylistic/eslint-plugin-ts';
+import parserTs from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
+import ts from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs['flat/recommended'],
-	prettier,
-	...svelte.configs['flat/prettier'],
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
-		}
-	},
-	{
-		files: ['**/*.svelte'],
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs['flat/recommended'],
+  prettier,
+  ...svelte.configs['flat/prettier'],
+  {
+    plugins: {
+      '@stylistic/ts': stylisticTs
+    },
+    languageOptions: {
+      parser: parserTs,
+    },
+    rules: {
+      '@stylistic/ts/indent': ['error', 2],
+    }
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ['**/*.svelte'],
 
-		languageOptions: {
-			parserOptions: {
-				parser: ts.parser
-			}
-		}
-	},
-	{
-		ignores: ['build/', '.svelte-kit/', 'dist/']
-	}
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser
+      }
+    }
+  },
+  {
+    ignores: ['build/', '.svelte-kit/', 'dist/']
+  }
 ];
--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -5,22 +5,22 @@ import type { Config } from 'tailwindcss';
 import { skeleton } from '@skeletonlabs/tw-plugin';
 
 const config = {
-	darkMode: 'class',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		skeleton({
-			themes: { preset: ["vintage"] }
-		})
-	]
+  darkMode: 'class',
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    join(require.resolve(
+      '@skeletonlabs/skeleton'),
+    '../**/*.{html,js,svelte,ts}'
+    )
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    skeleton({
+      themes: { preset: ["vintage"] }
+    })
+  ]
 } satisfies Config;
 
 export default config;
